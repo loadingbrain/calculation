087131da5a4d78cba1bcfaf3eb6cdbaec5ab3593
fox <141747318+loadingbrain@users.noreply.github.com>
2023-08-23 12:47:15 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
-document.getElementById('calculate').addEventListener('click', () => {
-    const birthdate = new Date(document.getElementById('birthdate').value);
-    const currentDate = new Date(document.getElementById('currentDate').value);
-    
-    const ageInMilliseconds = currentDate - birthdate;
-    const ageDate = new Date(ageInMilliseconds);
+document.addEventListener("DOMContentLoaded", function () {
+    const calculateBtn = document.getElementById("calculateBtn");
+    const ageDisplay = document.querySelector(".age-display");
+    const displayBg = document.querySelector(".display-bg");
   
-    const years = ageDate.getUTCFullYear() - 1970;
-    const months = ageDate.getUTCMonth();
-    const days = ageDate.getUTCDate() - 1;
+    calculateBtn.addEventListener("click", function () {
+      const birthday = new Date(document.getElementById("birthday").value);
+      const currentDate = new Date(document.getElementById("currentDate").value);
   
-    const calculatedAgeElement = document.getElementById('calculatedAge');
-    calculatedAgeElement.textContent = years;
+      const age = calculateAge(birthday, currentDate);
   
-    const calculatedMonthsElement = document.getElementById('calculatedMonths');
-    calculatedMonthsElement.textContent = months;
+      document.getElementById("age").textContent = formatAge(age);
+      ageDisplay.style.display = "block";
+      displayBg.style.display = "block";
   
-    const calculatedDaysElement = document.getElementById('calculatedDays');
-    calculatedDaysElement.textContent = days;
+      calculateBtn.style.backgroundColor = "white";
+      ageDisplay.style.boxShadow = "0 4px 10px rgba(0, 0, 0, 0.2)";
   
-    const resultElement = document.getElementById('result');
-    resultElement.classList.add('visible');
+      setTimeout(() => {
+        ageDisplay.style.animation = "none";
+      }, 1000);
+    });
+  
+    displayBg.addEventListener("click", function () {
+      ageDisplay.style.display = "none";
+      displayBg.style.display = "none";
+    });
+  
+    function calculateAge(birthday, currentDate) {
+      const yearDiff = currentDate.getFullYear() - birthday.getFullYear();
+      const monthDiff = currentDate.getMonth() - birthday.getMonth();
+      const dayDiff = currentDate.getDate() - birthday.getDate();
+  
+      return {
+        years: yearDiff,
+        months: monthDiff,
+        days: dayDiff
+      };
+    }
+  
+    function formatAge(age) {
+      const { years, months, days } = age;
+      return `${years} years, ${months} months, and ${days} days`;
+    }
   });
   
